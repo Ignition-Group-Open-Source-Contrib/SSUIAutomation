bbb871dfd703bb581f97c954b0926be2333ace8e
RohitKuma-Git <rkumar956@ignitiongroup.co.za>
2025-05-08 16:41:54 +0530

    Add Playwright config file and change the script in yaml file.

---
+++ playwright.config.js
@@ -0,0 +1,28 @@
+import { defineConfig } from '@playwright/test';
+import dotenv from 'dotenv';
+dotenv.config();
+
+console.log("Playwright config loaded!");
+
+export default defineConfig({
+  testDir: './tests',
+  timeout: 120000,
+  projects: [
+    {
+      name: 'uat', // Must match --project=uat
+      use: {
+        headless: true, // Or false for debugging
+        browserName: 'chromium',
+        baseURL: process.env.UAT_URL || 'http://t2.silversurfer.ignitiongroup.co.za',
+      },
+    },
+    {
+      name: 'prod',
+      use: {
+        headless: true,
+        browserName: 'chromium',
+        baseURL: process.env.PROD_URL || 'https://silversurfer.ignitiongroup.co.za/Auth',
+      },
+    }
+  ]
+});
