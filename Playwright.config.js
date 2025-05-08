b970f7cc5f270bf14b7073dce44e22affe216f54
RohitKuma-Git <rkumar956@ignitiongroup.co.za>
2025-05-08 12:45:37 +0530

    Add console log in Playwright.config file

--- Playwright.config.js
+++ Playwright.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from '@playwright/test';
 import dotenv from 'dotenv';
 dotenv.config();
 
+console.log("✅ Playwright config loaded!");
+
 export default defineConfig({
   testDir: './tests',
   timeout: 120000,
